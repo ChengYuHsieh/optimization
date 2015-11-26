2565da5bb783dbd82fb92b630ffe90df69c36d53
ChengYuHsieh <danielhsieh01@gmail.com>
2015-11-26 11:58:10 +0800

    subgradient method done

--- sub1.py
+++ sub1.py
@@ -1,76 +1,122 @@
 import numpy as np
-def main():
-	numM = raw_input("Number of workers having multiple skills: ")
-	numS = raw_input("Number of workers having a single skill: ")
-	timeI = raw_input("Number of time segments: ")
-	classtype = raw_input("Number of classtype  input ")
-	fakeData = subProb1Dat(int(numM), int(numS), int(timeI))
-	newWRA = solver1(fakeData[0], fakeData[1], fakeData[2],fakeData[4])
-	newWU  = solver2(fakeData[1], fakeData[3],fakeData[5])
-	print 'updated WRA:'
-	print newWRA
-	print 'updated w/u:'
-	print newWU0
+import sub3 as s3
+import math
 
-def subProb1Dat(numM,numS,timeI):
-	### initialize lagrangian multipliers
-	lagMul1 = np.random.randint(20, size=numM*timeI).reshape(numM, timeI)
-	lagMul2 = np.random.randint(20, size=numS*timeI).reshape(numS, timeI)
-	print 'lagMul1: '
-	print lagMul1
-	print 'lagMul2: '
-	print lagMul2
-	### random ndarray in the range of 20
-	wra = np.random.randint(20, size=numM*numS*timeI).reshape(numM, numS, timeI)
-	wu  = np.random.randint(20, size=numS*timeI).reshape(numS, timeI)
-	wrb= wra+np.random.randint(20, size=numM*numS*timeI).reshape(numM, numS, timeI)
-	wub = wu+np.random.randint(20, size=numS*timeI).reshape(numS, timeI)
-	return [lagMul1, lagMul2, wra, wrb, wu ,wub]
+def main():
+    numM = int(raw_input("Number of multiple skills: "))
+    numS = int(raw_input("Number of single skills: "))
+    timeI = int(raw_input("Number of time intervals: "))
+    numC = int(raw_input("Number of class types: "))
+
+    # initialize lagrangian multipliers
+    lagMul1 = np.zeros((numM, timeI))
+    lagMul2 = np.zeros((numS, timeI))
+    params = paramsGen(numM, numS, numC, timeI)
+    delta = 1
+    for i in range(1,10000):
+        delta = delta / math.sqrt(i)
+        #solver1, 2
+        newWRA = solver1(lagMul1, lagMul2, params[0], params[1])
+        newWU = solver2(lagMul2, params[2], params[3])
+        print 'updated WRA:'
+        print newWRA
+        print 'updated w/u:'
+        print newWU
+
+        #solver3, 4
+        newWAs = s3.solver3(numS, numC, lagMul2, params[6], params[4])
+        newWAm = s3.solver3(numM, numC, lagMul1, params[6], params[5])
+        print 'updated WAs:'
+        print newWAs
+        print 'updated WAm:'
+        print newWAm
+
+        newLMs = subgradientsolver(lagMul1, lagMul2, newWRA, newWU, newWAs, newWAm,
+                                params[6], params[7], delta)
+
+def paramsGen(numM, numS, numC, timeI):
+    # random ndarray in the range of 20
+    wra = np.zeros((numM, numS, timeI))
+    # wrb is the upper bound for wra
+    wrb = np.random.randint(20, size=numM*numS).reshape(numM, numS)
+    wu = np.zeros((numS, timeI))
+    # wu is the upper bound for wu
+    wub = np.random.randint(20, size=numS*timeI).reshape(numS, timeI)
+    # for sub3 and sub4
+    numSUBs = np.random.randint(20, size=numS)
+    numMUBs = np.random.randint(20, size=numM)
+    # class Matrix
+    classMat = np.random.randint(2, size=numC*timeI).reshape(numC,timeI)
+    # demand Matrix
+    demand = np.random.randint(5, size=numS*timeI).reshape(numS, timeI)
+
+    return [wra, wrb, wu, wub, numSUBs, numMUBs, classMat, demand]
+
 
 def solver1(lagMul1, lagMul2, wra, wrb):
-	dim1 = lagMul1.shape
-	dim2 = lagMul2.shape
-	for i in range(dim1[0]):
-		for j in range(dim2[0]):
-			for k in range(dim1[1]):
-					if lagMul1[i,k] > lagMul2[j,k]:
-						wra[i,j,k] = 0
-					else:
-						wra[i,j,k] = wrb[i,j] #some big value
-	return wra
-def solver2(lagMul2,wu,wub):
-	dim2 = lagMul2.shape
-	for i in range(dim2[0]):
-		for j in range(dim2[1]):
-			if lagMul2[i,j]<1:
-				wu[i,j]=0
-			else:
-				wu[i,j]=wub[i,j]
-	return wu
-# def subgradientsolver(lagMul1,lagMul2,newwra,newwu,newwas,newwam,classinfom,demand,delta):
-# 	dim1=lagMul1.shape
-# 	dim2=lagMul2.shape
-# 	dim3=classinfom.shape
-# 	for i in range(dim1[0]):
-# 		for j in range(dim1[1]):
-# 			wrasum[i,j]=0
-# 			for k in range(dim2[0]):
-# 				wrasum[i,j]=newwra[i,k,j]
-# 			for c in range(dim3[0]):
-# 				wamc[i,j]=newwam[i,c]*classinfom[c,j]
-# 			subgrad1[i,j]=wrasum[i,j]-wamc[i,j]
-            if subgrad1[i,j]=
-# 			newlagMul1[i,j]=lagMul1[i,j]+delta*subgrad1[i,j]
-# 	for i in range(dim2[0]):
-# 		for j in range(dim2[1]):
-# 			wrasum[i,j]=0
-# 			for k in range(dim1[0]):
-# 				wrasum[i,j]=newwra[k,i,j]
-# 			for c in range(dim3[0]):
-# 				wasc[i,j]=newwas[i,c]*classinfom[c,j]
-# 			subgrad2[i,j]=demand[i,j]-wrasum[i,j]-wasc[i,j]-newwu[i,j]
-# 			newlagMul2[i,j]=lagMul2[i,j]+delta*subgrad2[i,j]
+    dim1 = lagMul1.shape
+    dim2 = lagMul2.shape
+    for i in range(dim1[0]):
+        for j in range(dim2[0]):
+            for k in range(dim1[1]):
+                if lagMul1[i, k] > lagMul2[j, k]:
+                    wra[i, j, k] = 0
+                else:
+                    wra[i, j, k] = wrb[i, j]
+    return wra
 
-#     return [newlagMul1,newlagMul2]
+
+def solver2(lagMul2, wu, wub):
+    dim2 = lagMul2.shape
+    for i in range(dim2[0]):
+        for j in range(dim2[1]):
+            if lagMul2[i, j] < 1:
+                wu[i, j] = 0
+            else:
+                wu[i, j] = wub[i, j]
+    return wu
+
+
+def subgradientsolver(lagmul1, lagmul2, newwra, newwu, newwas, newwam,
+                      classinfom, demand, delta):
+    dim1 = lagmul1.shape
+    dim2 = lagmul2.shape
+    dim3 = classinfom.shape
+    subgrad1 = np.zeros((dim1[0], dim1[1]))
+    wrasum = np.zeros((dim1[0], dim1[1]))
+    wamc = np.zeros((dim1[0], dim1[1]))
+    newlagmul1 = np.zeros((dim1[0], dim1[1]))
+
+    for i in range(dim1[0]):
+        for j in range(dim1[1]):
+            for k in range(dim2[0]):
+                wrasum[i, j] += newwra[i, k, j]
+            for c in range(dim3[0]):
+                wamc[i, j] = newwam[i, c]*classinfom[c, j]
+            subgrad1[i, j] = wrasum[i, j]-wamc[i, j]
+            newlagmul1[i, j] = lagmul1[i, j]+delta*subgrad1[i, j]
+            if newlagmul1[i, j] < 0:
+                newlagmul1[i, j] = 0
+    print subgrad1
+
+    subgrad2 = np.zeros((dim2[0], dim2[1]))
+    wrasum = np.zeros((dim2[0], dim2[1]))
+    wasc = np.zeros((dim2[0], dim2[1]))
+    newlagmul2 = np.zeros((dim2[0], dim2[1]))
+
+    for i in range(dim2[0]):
+        for j in range(dim2[1]):
+            for k in range(dim1[0]):
+                wrasum[i, j] = newwra[k, i, j]
+            for c in range(dim3[0]):
+                wasc[i, j] = newwas[i, c]*classinfom[c, j]
+            subgrad2[i, j] = demand[i, j]-wrasum[i, j]-wasc[i, j]-newwu[i, j]
+            newlagmul2[i, j] = lagmul2[i, j]+delta*subgrad2[i, j]
+            if newlagmul2[i, j] < 0:
+                newlagmul2[i, j] = 0
+    print subgrad2
+
+    return [newlagmul1, newlagmul2]
+
 if __name__ == "__main__":
-    main()    
+    main()
